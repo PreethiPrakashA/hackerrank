a8402b5c87e7e3ec03ad7e32349fdd2275ceb432
PreethiPrakashA <40957676+PreethiPrakashA@users.noreply.github.com>
2020-04-26 23:11:45 +0530

    solution.js

---
+++ problem-solving/Day 1: Let and Const.js
@@ -0,0 +1,13 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    var r  = readLine();
+    var area = PI * r * r;
+    var perimeter = 2 * PI * r;
+    
+    // Print the area of the circle:
+    console.log(area);
+    console.log(perimeter);
+    
+    // Print the perimeter of the circle:
+}
